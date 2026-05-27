a0a66c292ea7e1426314ac6450694c36d3c21f68
Sigit Permadi <116578793+permadi1053@users.noreply.github.com>
2026-05-28 04:40:20 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,19 @@
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    // Jika request mengarah ke sub-path .well-known atau _matrix, oper langsung ke Tunnel Anda
+    if (
+      url.pathname.startsWith('/.well-known/') || 
+      url.pathname.startsWith('/_matrix/')
+    ) {
+      return fetch(request);
+    }
+
+    // Default response jika mengakses domain luar path Matrix
+    return new Response("Hello world", {
+      headers: { "content-type": "text/plain" },
+    });
+  },
+};
 
